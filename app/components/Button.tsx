fc9141dd56adad830f90ccbe01015021c09523fa
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-18 15:41:06 +0900

    Buttonコンポーネントのタイポ修正

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,17 +2,19 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 
 type ButtonProps = {
+  type: 'submit' | 'button';
     text: string;
     onClick?: () => void;
 };
 
 export default function Btn({
+  type,
     text,
     onClick,
 }: ButtonProps) {
   return (
     <>
-      <Button variant="outlined" onClick={onClick}>{text}</Button>
+      <Button variant="outlined" onClick={onClick} type={type}>{text}</Button>
     </>
   );
 }

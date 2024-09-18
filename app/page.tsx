1bb06f72e799460f723bf7f9d1306d688970b6f7
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-18 15:41:28 +0900

    新規登録ボタンから新規登録ページに遷移

--- app/page.tsx
+++ app/page.tsx
@@ -2,43 +2,43 @@
 
 import title from "./images/title.png"
 import Image from "next/image";
-import Btn from "./conponemts/Button/page";
+import Btn from "./components/Button";
 
 export default function Home() {
 
   const handleClick = () => {
-    window.location.href = "/SelectFavorite";
+    window.location.href = "/newRegistration";
   }
 
   return (
     <div style={{
       height: "100vh",
     }}>
-    <main style={
-      {
+      <main style={
+        {
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexFlow: "column",
+          height: "90%",
+        }}>
+        <p>好きな情報をまとめて管理</p>
+        <Image src={title} alt="title" width={500} height={500} />
+        <div style={{
+          display: "flex",
+          flexFlow: "column",
+        }}>
+          <Btn text={"ログイン"} type={"button"}/>
+          <Btn type={"button"} text={"新規登録"} onClick={handleClick} />
+        </div>
+      </main>
+      <footer style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        flexFlow: "column",
-        height: "90%",
-    }}>
-      <p>好きな情報をまとめて管理</p>
-      <Image src={title} alt="title" width={500} height={500} />
-      <div style={{
-        display: "flex",
-        flexFlow: "column",
+        height: "10%",
+        borderTop: "1px solid #ccc",
       }}>
-      <Btn text={"ログイン"}/>
-      <Btn text={"新規登録"} onClick={handleClick}/>
-      </div>
-    </main>
-    <footer style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "10%",
-      borderTop: "1px solid #ccc",
-    }}>
         <p>@watnowハッカソン</p>
       </footer>
     </div>

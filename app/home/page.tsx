0356e0b9231bc0214fe6abc8d8ba877d875c43f0
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-20 06:17:16 +0900

    一覧ページのデータのやり取り完成

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,106 +1,118 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Link from "next/link";
 
 const Home = () => {
-  const sampleData = [
-    {
-      title: "YOASOBI",
-      image:
-        "http://www.sonymusic.co.jp/adm_image/common/artist_image/90000/90055/profile_image/56696__0_0_0.jpg",
-      tag: "音楽",
-    },
-    {
-      title: "YOASOBI",
-      image:
-        "http://www.sonymusic.co.jp/adm_image/common/artist_image/90000/90055/profile_image/56696__0_0_0.jpg",
-      tag: "音楽",
-    },
-    {
-      title: "YOASOBI",
-      image:
-        "http://www.sonymusic.co.jp/adm_image/common/artist_image/90000/90055/profile_image/56696__0_0_0.jpg",
-      tag: "アニメ",
-    },
-  ];
+  const [groupedData, setGroupedData] = useState<{ [key: string]: any[] }>({});
+  const [loading, setLoading] = useState(true); // Loading state
 
-  const groupedData = sampleData.reduce((acc, item) => {
-    if (!acc[item.tag]) {
-      acc[item.tag] = [];
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const userEmail = localStorage.getItem('userEmail');
+      if (userEmail) {
+        await getFavorites({ email: userEmail });
+      } else {
+        setLoading(false); // No user email, stop loading
+      }
+    };
+    fetchGenres();
+  }, []);
+
+  const getFavorites = async (data) => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-user-oshi-genres`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: data.email }),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        const oshiArray = responseData.oshi; // Access the oshi array
+
+        const grouped = oshiArray.reduce((acc, item) => {
+          if (!acc[item.genre]) {
+            acc[item.genre] = [];
+          }
+          acc[item.genre].push(item);
+          return acc;
+        }, {});
+
+        setGroupedData(grouped);
+      } else {
+        console.error('Failed to fetch genres');
+      }
+    } catch (error) {
+      console.error('Error fetching genres:', error);
+    } finally {
+      setLoading(false); // Stop loading regardless of success or failure
     }
-    acc[item.tag].push(item);
-    return acc;
-  }, {});
+  };
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <Header />
-      <div
-        style={{
-          backgroundColor: "#F5F5F5",
-          height: "calc(100vh - 90px)",
-        }}
-      >
-        {Object.keys(groupedData).map((tag) => (
-          <div key={tag} style={{ fontFamily: 'JPFont', margin: "30px 33px" }}>
-            <h2 style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{tag}</h2>
-            {groupedData[tag].map((data) => (
-              <Link href={`/favoriteNote/${data.title}`}>
-                <div
-                  key={data.title}
-                  style={{
-                    height: '100px',
-                    display: "flex",
-                    alignItems: "center",
-                    padding: "4px 0",
-                  }}
-                >
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: 'center',
-                      width: "130px",
-                      height: "150px",
-                    }}
-                  >
-                    <img
-                      src={data.image}
-                      alt={data.title}
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <Header />
+          <div style={{ backgroundColor: "#F5F5F5", height: "calc(100vh - 90px)", overflowY: "auto" }}>
+            {Object.keys(groupedData).map((tag) => (
+              <div key={tag} style={{ fontFamily: 'JPFont', margin: "30px 33px" }}>
+                <h2 style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{tag}</h2>
+                {groupedData[tag].map((data) => (
+                  <Link key={data.oshi_name} href={`/favoriteNote/${data.oshi_name}`}>
+                    <div
                       style={{
-                        maxWidth: '100%',
-                        borderRadius: "10%",
-                        // marginRight: "16px",
-                      }}
-                    />
-                  </div>
-                  <div style={{ display: "flex", flexDirection: "column" }}>
-                    <p
-                      style={{
-                        fontSize: "1.2rem",
-                        fontWeight: "bold",
-                        margin: "4px 20px",
+                        height: '100px',
+                        display: "flex",
+                        alignItems: "center",
                       }}
                     >
-                      {data.title}
-                    </p>
-                  </div>
-                </div>
-              </Link>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: 'center',
+                          width: "100px",
+                          height: "150px",
+                        }}
+                      >
+                        <img
+                          src={data.image_url}
+                          alt={data.oshi_name}
+                          style={{
+                            maxWidth: '100%',
+                            borderRadius: '50%',
+                            width: '70px',
+                            height: '70px',
+                            objectFit: 'cover', // 画像を切り取って表示する
+                          }}
+                        />
+                      </div>
+                      <div style={{ display: "flex", flexDirection: "column", width:"100%" }}>
+                        <p
+                          style={{
+                            fontSize: "1.2rem",
+                            fontWeight: "bold",
+                            margin: "4px 20px",
+                          }}
+                        >
+                          {data.oshi_name}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+              </div>
             ))}
           </div>
-        ))}
-      </div>
-      <div
-        style={{
-          height: "",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "flex-end",
-        }}
-      >
+        </>
+      )}
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "flex-end" }}>
         <button
           style={{
             position: "absolute",
@@ -108,25 +120,20 @@ const Home = () => {
             width: "76px",
             height: "76px",
             borderRadius: "50%",
-            background: "white", // 背景を白に
+            background: "conic-gradient(#EAC46A, #D5FCD5, #6C97EC, #0731FB, #7634DB, #FF44AA, #FF6E49, #FF8E03, #EAC463)",
             fontSize: "56px",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             cursor: "pointer",
-            border:
-              "9px solid conic-gradient(#EAC46A, #D5FCD5, #6C97EC, #0731FB, #7634DB, #FF44AA,#FF6E49,#FF8E03,#EAC463)", // 虹色のボーダー
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // 立体感
-            color: "transparent", // 文字自体を透明に
-            background:
-              "conic-gradient(#EAC46A, #D5FCD5, #6C97EC, #0731FB, #7634DB, #FF44AA,#FF6E49,#FF8E03,#EAC463)", // 虹色の背景
-            backgroundClip: "text", // テキストの部分にだけ背景を適用
-            WebkitBackgroundClip: "text", // Webkit対応
+            color: "transparent",
+            backgroundClip: "text",
+            WebkitBackgroundClip: "text",
             transition: "transform 0.2s",
           }}
           onClick={() => (window.location.href = "/selectFavorite")}
-          onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.1)")} // ホバー時に拡大
-          onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")} // 元のサイズに戻す
+          onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.1)")}
+          onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
         >
           +
         </button>
